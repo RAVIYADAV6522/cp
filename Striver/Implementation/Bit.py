12d2f9ad5e04ef34a4f24461e3f41c60269d5068
Ravi Yadav <yadavr74839@gmail.com>
2025-05-15 00:31:02 +0530

    triangle

--- Striver/Implementation/Bit.py
+++ Striver/Implementation/Bit.py
@@ -1,9 +1,9 @@
-t=int(input())
-count=0
-for i in range(t):
-    x=input()
-    if "++" in x:
-        count+=1 
-    else:
-        count-=1 
-print(count) 
+# t=int(input())
+# count=0
+# for i in range(t):
+#     x=input()
+#     if "++" in x:
+#         count+=1 
+#     else:
+#         count-=1 
+# print(count) 
---
+++ Striver/Maths/triangle.py
@@ -0,0 +1,7 @@
+t=int(input())
+for _ in range(t):
+  a,b,c,d=map(int,input().split(" "))
+  x=b
+  y=c
+  z=c 
+print(x,y,z,)
